d5d097c2aba3e6544595ae235c166abb88988a65
Simone Pietrelli <simone.pietrelli@gruppoglobalsistemi.it>
2020-10-16 14:44:07 +0200

    Test corretto

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -11,7 +11,7 @@ namespace Tests
         {
             string testString = "test";
 
-            Assert.AreEqual("test1", testString);
+            Assert.AreEqual("test", testString);
         }
     }
 }
